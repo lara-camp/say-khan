a940177084dcfe8eaced870809bad519b809aaa2
Hein Wai Yan Kyaw <heinwaiyankyaw2001@gmail.com>
2023-09-25 13:50:49 +0630

    [Modiy]Login Page

--- routes/hhz.php
+++ routes/hhz.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/login', function () {
     return view('user.login');
-});
-Route::get('/', function () {
-    return view('user.register');
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,25 @@ module.exports = {
         "./resources/**/*.vue",
     ],
     theme: {
+        colors: {
+            purple: "#5b2a86",
+            blue: "#6184d8",
+            green: "#85cb33",
+            white: "#f7f4ea",
+            gray: "#ded9e2",
+        },
+        fontFamily: {
+            sans: ["Poppins", "sans-serif"],
+        },
+        fontSize: {
+            h1: "48px",
+            h2: "40px",
+            h3: "32px",
+            b1: "32px",
+            b2: "28px",
+            b3: "24px",
+            b4: "18px",
+        },
         extend: {},
     },
     plugins: [],
