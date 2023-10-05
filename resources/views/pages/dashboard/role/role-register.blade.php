f0fc093045cc56a00fdbc420a7d24032dc55ec3b
dafolk <kaungsoesan01@gmail.com>
2023-10-06 00:50:28 +0630

    [Modify] role ui change

--- resources/views/pages/dashboard/role/role-register.blade.php
+++ resources/views/pages/dashboard/role/role-register.blade.php
@@ -11,7 +11,7 @@
             <h2 class="text-b2 text-purple mb-10">fill this form to register role</h2>
             <form action="{{ route('role.store') }}" method="POST" class="w-full flex flex-col gap-3">
                 <x-input-field id="role-name" name="role-name" placeholder="Enter Role Name" color="red" />
-                <x-input-field id="role - type" name="role - type" placeholder="Enter Type" color="red" />
+                <x-input-field id="role-type" name="role-type" placeholder="Enter Type" color="red" />
                 <x-button bgColor="purple" textColor="white" text="Register" />
             </form>
             <a href="{{ url('/admin/role') }}" class="w-full underline text-blue text-right pt-5">view role</a>
