b1dae47a6bc95283b48d03dcb1f8177206d0c3ab
Aungkhant-xt4 <akhant074@gmail.com>
2023-10-06 14:38:43 +0630

    doctor index

--- resources/views/doctor/index.blade.php
+++ resources/views/doctor/index.blade.php
@@ -1,11 +1,6 @@
 @extends('doctor.layouts.master')
 @section('content')
 
-<div class="d-flex align-items-center justify-content-between">
-    <h1 class="mb-0 ">Doctor</h1>
-    <a href="{{ route('doctor.create') }}" class="btn btn-primary">New Doctor</a>
-</div>
-
 @if (Session::has('success'))
 <div class="alter alert-warning" doctor="alert">
     {{ Session::get('success') }}
@@ -16,57 +11,34 @@
         {{ session('error') }}
     </div>
 @endif
-@auth
-<h2>Welcome {{ Auth::user()->name }}</h2>
-<a href="{{ route('logout') }}">Logout</a>
-@endauth
+
+
+<h2>Welcome {{ Auth::guard('doctor')->user()->name }}</h2>
 
 {{-- table --}}
 <table class="table table-hover">
     <thead class="table-primary">
         <tr>
-            <th scope="col">#</th>
-            <th scope="col">Name</th>
-            <th scope="col">Speciality</th>
-            <th scope="col">Phone</th>
-            <th scope="col">Email</th>
-            <th scope="col">Address</th>
             <th scope="col">Action </th>
         </tr>
     </thead>
     <tbody>
-    @if ($doctors->count() > 0)
-        @foreach ($doctors as $doctor)
         <tr>
-            <td class="align-items-center">{{ $loop->iteration }}</td>
-            <td class="align-items-center">{{ $doctor->name }}</td>
-            <td class="align-items-center">{{ $doctor->speciality }}</td>
-            <td class="align-items-center">{{ $doctor->phone }}</td>
-            <td class="align-items-center">{{ $doctor->email }}</td>
-            {{-- <td class="align-items-center">{{ $doctor->password }}</td> --}}
-            <td class="align-items-center">{{ $doctor->address }}</td>
             <td class="align-items-center">
                 <div class="btn-group" doctor="group">
-                    <a href="{{ route('doctor.edit', encrypt($doctor->id)) }}" type="button" class="btn btn-danger">Edit {{ $doctor->id }}</a>
-                    <a href="{{ route('feedback_create', encrypt($doctor->id)) }}" type="button" class="btn btn-danger">Give Feedback</a>
-                    <a href="{{ route('clinic_subscription_create', encrypt($doctor->id)) }}" type="button" class="btn btn-danger">Buy Subscription</a>
-                    <form method="post" action="{{ route('doctor.destroy',encrypt($doctor->id)) }}">
-                        @csrf
-                        @method('delete')
-                        <button type="submit" class="btn btn-success" onclick="return confirm('i want to delete this')" alt="">Delete</button>
-                    </form>
+                    <a href="{{ route('feedback_create', encrypt(auth()->guard('doctor')->user()->id)) }}" type="button" class="btn btn-danger">Give Feedback</a>
+                    <a href="{{ route('doctor.edit', encrypt(auth()->guard('doctor')->user()->id)) }}" type="button" class="btn btn-danger">Edit</a>
+                    <a href="{{ route('clinic_subscription_create', encrypt(auth()->guard('doctor')->user()->id)) }}" type="button" class="btn btn-danger">Buy Subscription</a>
                 </div>
             </td>
         </tr>
-        @endforeach
-        @else
-        <tr>
-            <td class="text-center" colspan="2">
-                There is no doctors
-            </td>
-        </tr>
-        @endif
     </tbody>
 </table>
+@if (auth()->guard('doctor')->check())
+    <form action="{{ route('user.logout') }}" method="POST">
+        @csrf
+        <button type="submit" class="btn btn-link">Logout</button>
+    </form>
+@endif
 
 @endsection
